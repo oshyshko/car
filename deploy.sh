045bd64b0ac6fc6983125c5202c3cd8ccc0e4f81
Oleksandr Shyshko <oshyshko@gmail.com>
2014-12-28 04:19:06 +0200

    Muted failing "killall java"

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,9 @@ ssh $PI_USER@$PI_HOST "mkdir -p $PI_JAR_DIR"
 scp -q $JAR $PI_USER@$PI_HOST:$PI_JAR
 
 # kill previous Java, if there is any
+set +e
 ssh $PI_USER@$PI_HOST "killall java"
+set -e
 
 # run
 ssh $PI_USER@$PI_HOST "java -jar $PI_JAR"
